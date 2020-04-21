545728050c1ffac0092ab678a3a5680d3d5085fe
ahmad-aljabali <ahmad-aljabali@protonmail.com>
2020-04-22 02:13:25 +0300

    latest version of tested

--- ROS_addon/ROS_PT_object_settings.py
+++ ROS_addon/ROS_PT_object_settings.py
@@ -5,7 +5,7 @@ bpy.types.Object.topic = bpy.props.StringProperty(name="Topic")
 bpy.types.Object.mode_type = bpy.props.EnumProperty(items = [("none", "None", "", 1),("subscriber", "Subscriber", "", 2),("publisher", "Publisher", "", 3)],
                                                          description="Mode for ROS topic")
 
-bpy.types.Object.message_type = bpy.props.EnumProperty(items = [("Point", "Point", "", 1), ("Quaternion", "Quaternion (Rotation)", "", 2),("Pose", "Pose", "", 3)],
+bpy.types.Object.message_type = bpy.props.EnumProperty(items = [("Pose", "Pose", "", 1), ("Point", "Point", "", 2), ("Quaternion", "Quaternion", "", 3)],
                                                          description="Message type used by ROS")
 
 class ROS_PT_object_settings(bpy.types.Panel):
@@ -17,16 +17,17 @@ class ROS_PT_object_settings(bpy.types.Panel):
 
     def draw(self, context):
         layout = self.layout
-        col = layout.column()
         if bpy.context.scene.ros_run_bool == False:
             if not context.object or context.object.type == "CAMERA":
                 layout.label(text="SLECT OBJECT")
             elif context.object.type != "CAMERA":
-                col.label(text="Mode:")
-                col.prop(context.object, "mode_type", text="")
-                col.label(text="Topic:")
-                col.prop(context.object, "topic", text="")
-                col.label(text="Message Type:")
-                col.prop(context.object, "message_type", text="")
+                layout.label(text="Mode:")
+                row = layout.row()
+                row.prop(context.object, "mode_type", expand=True)
+                layout.label(text="Topic:")
+                layout.prop(context.object, "topic", text="")
+                layout.label(text="Message Type:")
+                row = layout.row()
+                row.prop(context.object, "message_type", expand=True)
         elif bpy.context.scene.ros_run_bool == True:
             layout.label(text="STOP to change settings")
